f72ac73afe420b8b65cf3515fa668c0acef8791d
Kotryna Šlepetytė <49883485+sniegapele@users.noreply.github.com>
2020-02-13 17:04:05 +0200

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(value = "*")
 public class DemoController {
 
     public static final String BE_SUCCESS_MESSAGE = "BE works ";
